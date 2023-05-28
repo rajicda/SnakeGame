b8c6468f927acc4d0b07a4cde206fbdfaade16af
rajicda <rajic.danijel11@gmail.com>
2023-05-28 13:46:48 +0200

    added option to move snake and not go backwards

--- main.py
+++ main.py
@@ -11,10 +11,15 @@ screen.title("Snake Game")
 screen.bgcolor("black")
 screen.tracer(0)
 
+screen.listen()
+screen.onkey(key="Up", fun=snake.up)
+screen.onkey(key="Down", fun=snake.down)
+screen.onkey(key="Left", fun=snake.left)
+screen.onkey(key="Right", fun=snake.right)
 
 while not game_over:
     screen.update()
-    time.sleep(1)
+    time.sleep(0.1)
 
     snake.move()
 
--- snake.py
+++ snake.py
@@ -1,19 +1,26 @@
 from turtle import Turtle
 
+MOVE_DISTANCE = 20
+NUMBER_OF_STARTING_SNAKES = 3
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
 
 class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         move_back = 0
-        for _ in range(3):
+        for _ in range(NUMBER_OF_STARTING_SNAKES):
             turtle = Turtle("square")
             turtle.penup()
             turtle.color("white")
             turtle.setx(move_back)
-            turtle.speed(1)
             move_back -= 20
             self.segments.append(turtle)
 
@@ -22,4 +29,20 @@ class Snake:
             x = self.segments[segment_number - 1].xcor()
             y = self.segments[segment_number - 1].ycor()
             self.segments[segment_number].goto(x, y)
-        self.segments[0].forward(20)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
